9ffe89168b46228f69d81cc0dcf3fa4f2814c06c
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-05-10 18:17:24 +0300

    ADD: selector and dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,26 @@
-export const Counter = ({ counter, inc, dec, rnd }) => {
+import { useDispatch, useSelector } from "react-redux";
+import {inc, dec, rnd} from '../actions'
+
+export const Counter = () => {
+
+  // получаем значениен из глобального стэйта
+  const counter = useSelector( state => state.value)
+
+
+  // достаем dispatch для работы с редьюсером
+  const dispatch = useDispatch()
+
+
   return (
     <div className="jumbotron">
       <h1>{counter}</h1>
-      <button onClick={dec} className="btn btn-primary">
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
         DEC
       </button>
-      <button onClick={inc} className="btn btn-primary">
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
         INC
       </button>
-      <button onClick={rnd} className="btn btn-primary">
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">
         RND
       </button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { legacy_createStore as createStore, bindActionCreators } from "redux";
-import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
 import { reducer } from "./reducer";
-/* import { inc, dec, rnd } from './actions' */
-import * as actions from "./actions";
-/* import { Counter } from "./components/Counter"; */
 import App from "./components/App";
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-const update = () => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-
-    <Provider store={store}>
-      <App />
-    </Provider>
-
-  );
-};
-
-update();
-subscribe(update);
-
-
-    {/* <Counter
-      counter={getState().value}
-      inc={inc}
-      dec={dec}
-      rnd={() => {
-        const value = Math.floor(Math.random() * 10);
-        rnd(value);
-      }}
-    /> */}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
