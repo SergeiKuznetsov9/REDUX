3af88a2c4ec6b2d59644b99116e902c0f4545f5f
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-05-15 12:36:43 +0300

    ADD: example with toolkit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useSelector } from 'react-redux';
+import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+
 function App() {
-  return (
+
+  const {status, error} = useSelector(state => state.todos)
+
+  return (<>
+  <InputField />
+  <br/>
+  <br/>
+
+
+  {status === 'loading' && <h1>Loading...</h1>}
+  {error && <h1>Error occured: {error}</h1>}
     <div className="App">
-      All Works
+      <TodoList />
     </div>
-  );
+    </>);
 }
 
 export default App;
